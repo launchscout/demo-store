53b308376772b359d630f9567ca8967c3b43947a
Chris Nelson <superchrisnelson@gmail.com>
2023-10-31 12:12:02 -0400

    demo defaults to demo data

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-# launch-cart Demo Store
+# Launch Elements Demo Store
 
-This is the demo store for [launch-cart](https://launch-cart.fly.dev)
+This is the demo store for [Launch Elements](https://elements.launchscout.com)
+
+# Running with example data
+
+To run the demo store with example data from a test store and test stripe account:
+
+```
+npm install
+npm start
+```
+
+# Using your own Stripe Account
+
+1. Sign up for an account at `elements.launchscout.com`
+2. Connect your stripe account
+3. Create a store
+4. Copy the values from your store into .env
+5. `npm start`
+
+This will create product pages for each product it finds in your Stripe Account. If you don't have any products, you won't see anything :)
+
+
--- src/_data/prices.js
+++ src/_data/prices.js
@@ -1,15 +1,21 @@
 const environment = process.env.CONTEXT
 const api_key = process.env.STRIPE_API_KEY;
 const stripe = require("stripe")(api_key);
+const demoPrices = require('./demoPrices.js').prices;
 
 // One product can have multiple prices but for my use case, it's 1:1 so this works without having to massage the data
 // TODO: add Expanding data docs link
 async function getPrices() {
-  console.log('calling prices');
-  const response = await stripe.prices.list({
-    expand: ["data.product"],
-  });
-  return response.data.filter(price => price.active);
+  if (api_key === 'demo') {
+    return demoPrices;
+  } else {
+    console.log('calling prices');
+    const response = await stripe.prices.list({
+      expand: ["data.product"],
+    });
+    const prices = response.data.filter(price => price.active);
+    return prices;
+  }
 }
 
 module.exports = async function () {
